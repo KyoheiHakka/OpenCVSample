f765b74d3fcf5f17bed383e56bb19d60dc3ff1b7
KyoheiHakka <fmilkh49@i.softbank.jp>
2018-09-14 17:04:42 +0900

    Initial Commit

---
+++ OpenCVSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCVSample
+//
+//  Created by 八箇　恭平 on 2018/09/14.
+//  Copyright © 2018年 Kyohei Hakka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
